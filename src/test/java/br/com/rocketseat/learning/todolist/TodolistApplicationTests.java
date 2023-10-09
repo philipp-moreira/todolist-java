8d294b1c2ef72fd989c7495039625cf211f9eb8d
Philipp Moreira <psmdeveloper@gmail.com>
2023-10-09 20:17:21 -0300

    Criacao do projeto

---
+++ src/test/java/br/com/rocketseat/learning/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.learning.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
